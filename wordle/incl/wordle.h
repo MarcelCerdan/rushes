4a4390e1e708294d18a403ae7fc64b4e41185636
Maxime Thibault <mthibaul@z2r6p4.42lyon.fr>
2023-06-24 19:49:06 +0200

    Wordle

---
+++ wordle/incl/wordle.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wordle.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mthibaul <mthibaul@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/24 17:22:00 by mthibaul          #+#    #+#             */
+/*   Updated: 2023/06/24 17:22:00 by mthibaul         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef WORDLE_H
+# define WORDLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DICTIONNARY "../../data/words.txt"
+
+#endif
